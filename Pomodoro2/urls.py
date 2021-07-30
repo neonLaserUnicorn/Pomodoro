a2247552584e2790b6fa9f67630874e0c0c4118a
neonLaserUnicorn <astrargot@gmail.com>
2021-07-30 21:30:39 +0300

    Add requirements file

--- Pomodoro2/urls.py
+++ Pomodoro2/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('User.urls'), name='user')
+    path('user/', include('User.urls'), name='user'),
+    path('', include('User.urls'), name='user1')
 ]
